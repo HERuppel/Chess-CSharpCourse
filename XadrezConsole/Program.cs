79f7d999609ec9e9c6921299acd592712e53164b
HERuppel <heruppel2@gmail.com>
2021-08-06 02:27:26 -0300

    Chess Position class

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,19 +9,11 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
-                board.placePiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.placePiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.placePiece(new King(board, Color.Black), new Position(2, 4));
+            ChessPosition pos = new ChessPosition('a', 1);
 
-                Screen.printBoard(board);
-            }
-            catch (BoardException err)
-            {
-                Console.WriteLine(err);
-            }
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosition());
 
         }
     }
